75fd07e5493e423fa59e0b80d3faba6020176fe0
Dory <pemegomachado@gmail.com>
2023-01-17 21:36:07 +0100

    Fixed all of the validation layer "false" errors
    Turnsa out the API version was set to 1.0 instead of 1.3. I've also refactored some of the code and documented a bit around.

--- src/utils/device/device.cpp
+++ src/utils/device/device.cpp
@@ -59,11 +59,11 @@ namespace Engine {
 
         VkApplicationInfo appInfo = {};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "LittleVulkanEngine App";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
+        appInfo.pApplicationName = "LittleVulkanEngine App"; // App name
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0); // App version
+        appInfo.pEngineName = "No Engine"; // Engine name
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0); // Engine version
+        appInfo.apiVersion = VK_API_VERSION_1_3; // Vulkan API Version
 
         VkInstanceCreateInfo createInfo = {};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
--- src/utils/swapchain/swapchain.cpp
+++ src/utils/swapchain/swapchain.cpp
@@ -274,18 +274,18 @@ namespace Engine {
             VkImageCreateInfo imageInfo{};
             imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
             imageInfo.imageType = VK_IMAGE_TYPE_2D;
+            imageInfo.flags = 0;
+            imageInfo.format = swapChainDepthFormat;
             imageInfo.extent.width = swapChainExtent.width;
             imageInfo.extent.height = swapChainExtent.height;
             imageInfo.extent.depth = 1;
             imageInfo.mipLevels = 1;
             imageInfo.arrayLayers = 1;
-            imageInfo.format = swapChainDepthFormat;
-            imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
-            imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-            imageInfo.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
             imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+            imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+            imageInfo.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
             imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-            imageInfo.flags = 0;
+            imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
 
             device.createImageWithInfo(imageInfo,
                               VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
