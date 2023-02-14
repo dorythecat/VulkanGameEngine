2e9cfd5fb7dbe5177b6842d082393a127ffb4f36
Dory <pemegomachado@gmail.com>
2023-02-14 15:22:10 +0100

    Added Normal, TexCoord and Color to procedural Quad vertices
    What the title says.

--- src/utils/procedural/quad/quad.cpp
+++ src/utils/procedural/quad/quad.cpp
@@ -21,15 +21,31 @@ namespace Engine::Procedural {
             for (uint32_t z = 0; z <= resolution - 1; z++) {
                 Model::Vertex vertex1{};
                 vertex1.position = {x, 0.0f, z};
+                vertex1.color = {1.0f, 1.0f, 1.0f};
+                vertex1.normal = {0.0f, 1.0f, 0.0f};
+                vertex1.texCoord = {static_cast<float>(x) / static_cast<float>(resolution),
+                                    static_cast<float>(z) / static_cast<float>(resolution)};
 
                 Model::Vertex vertex2{};
                 vertex2.position = {x + 1, 0.0f, z};
+                vertex2.color = {1.0f, 1.0f, 1.0f};
+                vertex2.normal = {0.0f, 1.0f, 0.0f};
+                vertex2.texCoord = {static_cast<float>(x + 1) / static_cast<float>(resolution),
+                                    static_cast<float>(z) / static_cast<float>(resolution)};
 
                 Model::Vertex vertex3{};
                 vertex3.position = {x, 0.0f, z + 1};
+                vertex3.color = {1.0f, 1.0f, 1.0f};
+                vertex3.normal = {0.0f, 1.0f, 0.0f};
+                vertex3.texCoord = {static_cast<float>(x) / static_cast<float>(resolution),
+                                    static_cast<float>(z + 1) / static_cast<float>(resolution)};
 
                 Model::Vertex vertex4{};
                 vertex4.position = {x + 1, 0.0f, z + 1};
+                vertex4.color = {1.0f, 1.0f, 1.0f};
+                vertex4.normal = {0.0f, 1.0f, 0.0f};
+                vertex4.texCoord = {static_cast<float>(x + 1) / static_cast<float>(resolution),
+                                    static_cast<float>(z + 1) / static_cast<float>(resolution)};
 
                 if (uniqueVertices.count(vertex1) == 0) {
                     uniqueVertices[vertex1] = static_cast<uint32_t>(vertices.size());
